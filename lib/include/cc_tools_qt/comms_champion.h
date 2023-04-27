59213b54db992757b53c50e040fa099ece3763b6
Alex Robenko <arobenko@gmail.com>
2023-04-28 08:34:23 +1000

    Updated copyright.

--- lib/include/cc_tools_qt/comms_champion.h
+++ lib/include/cc_tools_qt/comms_champion.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2021 - 2022 (C). Alex Robenko. All rights reserved.
+// Copyright 2021 - 2023 (C). Alex Robenko. All rights reserved.
 //
 
 // This file is free software: you can redistribute it and/or modify
--- lib/include/cc_tools_qt/version.h
+++ lib/include/cc_tools_qt/version.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2018 - 2021 (C). Alex Robenko. All rights reserved.
+// Copyright 2018 - 2023 (C). Alex Robenko. All rights reserved.
 //
 // This file is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
