1050678e098e1a6057a18a16e3cf531e87ed996c
Alex Robenko <arobenko@gmail.com>
2023-06-13 08:32:57 +1000

    Require v5.2.1 of the COMMS library.

--- lib/include/cc_tools_qt/version.h
+++ lib/include/cc_tools_qt/version.h
@@ -77,7 +77,7 @@ constexpr unsigned version()
     return CC_TOOLS_QT_VERSION;
 }
 
-static_assert(COMMS_MAKE_VERSION(5, 2, 0) <= comms::version(),
+static_assert(COMMS_MAKE_VERSION(5, 2, 1) <= comms::version(),
     "The version of COMMS library is too old");
 
 } // namespace cc_tools_qt
