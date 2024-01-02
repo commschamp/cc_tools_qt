399653baa6871a3ab3c997937d2fbd8c31503e61
Alex Robenko <arobenko@gmail.com>
2024-01-03 08:57:26 +1000

    Updating appveyor and github actions configurations to compile with C++17 and later.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,15 +1,4 @@
-IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
-    set TOOLCHAIN=msvc14
-    set QT_SUBDIR=msvc2015
-    set QT_VER=5.11
-    IF "%PLATFORM%"=="x86" (
-        echo Performing x86 build in VS2015
-        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-    ) ELSE (
-        echo Performing amd64 build in VS2015
-        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-    )
-) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
+IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     set TOOLCHAIN=msvc15
     set QT_SUBDIR=msvc2017
     set QT_VER=5.13
