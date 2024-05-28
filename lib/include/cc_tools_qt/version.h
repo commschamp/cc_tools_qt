f71e41df39e76ce76884a0ee7b1a20fd75fb8045
Alex Robenko <arobenko@gmail.com>
2024-05-29 07:52:46 +1000

    Updating next release version to be v5.3.

--- lib/include/cc_tools_qt/version.h
+++ lib/include/cc_tools_qt/version.h
@@ -25,10 +25,10 @@
 #define CC_TOOLS_QT_MAJOR_VERSION 5U
 
 /// @brief Minor verion of the library
-#define CC_TOOLS_QT_MINOR_VERSION 2U
+#define CC_TOOLS_QT_MINOR_VERSION 3U
 
 /// @brief Patch level of the library
-#define CC_TOOLS_QT_PATCH_VERSION 2U
+#define CC_TOOLS_QT_PATCH_VERSION 0U
 
 /// @brief Macro to create numeric version as single unsigned number
 #define CC_TOOLS_QT_MAKE_VERSION(major_, minor_, patch_) \
